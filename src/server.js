b7e84a089dfbd85f59333e19e8910f449f344a14
hyejude <ivory0612@daum.net>
2022-12-31 15:57:00 +0900

    ~3.1

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 import express from "express";
 import http from "http";
 import { parse } from "path";
-import { Server } from 'socket.io';
+// import { Server } from 'socket.io';
+import SocketIO from "socket.io";
 import { instrument } from '@socket.io/admin-ui';
-// import WebSocket from "ws";
+import WebSocket from "ws";
 
 const app = express();
 
@@ -12,6 +13,9 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
+
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`)
 httpServer.listen(3000, handleListen);
